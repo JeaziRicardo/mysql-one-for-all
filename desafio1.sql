e6963ce01e17fd9ff182a85b876490408bff3ea1
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-07-25 18:57:41 -0300

    fix: corrige um dado na tabela 'songs'

--- desafio1.sql
+++ desafio1.sql
@@ -135,7 +135,7 @@ VALUES
   ('She Knows', 244, 3),
   ('Fantasy For Me', 100, 4),
   ('Celebration Of More', 146, 4),
-  ('Rock His Everything', 233, 4),
+  ('Rock His Everything', 223, 4),
   ('Home Forever', 231, 4),
   ('Diamond Power', 241, 4),
   ('Let\'s Be Silly', 132, 4),
